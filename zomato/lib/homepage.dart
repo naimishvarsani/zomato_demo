1e6f6a49b534818b84c05d75d0072e5bf89460db
naimishvarsani <79244625+naimishvarsani@users.noreply.github.com>
2022-06-20 21:15:28 +0530

    project setup

---
+++ zomato/lib/homepage.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
